bc1de2897e88db2ed9bab8eada0b7ce862143506
JeremieFa <167006796+JeremieFa@users.noreply.github.com>
2024-04-15 10:10:10 +0200

    Force bulma light theme

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,16 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+// force the bulma light theme : edit the index.html directly doesn't work :(
+function forceLightTheme() {
+  const htmlElements = document.getElementsByTagName("html")
+  if(htmlElements.length > 0) {
+    htmlElements[0].setAttribute("data-theme", "light");
+  }
+}
+
+forceLightTheme();
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
